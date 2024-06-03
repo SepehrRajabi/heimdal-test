b94a46c8342bbdcb6b5389d0044858303471ed2d
Sepehr Rajabi <sepehrrajabi478@gmail.com>
2024-06-03 15:44:04 +0330

    update for soroush

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,69 @@
-// import './App.css'
+import './App.css'
 
-// // importing the VideoJS Functional component
-// import VideoPlayer from './player'
+// importing the VideoJS Functional component
+import VideoPlayer from './player'
 
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>My Video App</h1>
-//         <VideoPlayer />
-//     </div>
-//   );
-// };
+const App = () => {
+  return (
+    <div>
+      <h1>My Video App</h1>
+        <VideoPlayer />
+    </div>
+  );
+};
 
-// export default App
-import React from 'react';
-import './index.css';
-import Hls from 'hls.js';
+export default App
+// import React from 'react';
+// import './index.css';
+// import Hls from 'hls.js';
 
-class App extends React.Component {
-  state = {
-    showModal: false,
-  };
-  myRef = React.createRef();
+// class App extends React.Component {
+//   state = {
+//     showModal: false,
+//   };
+//   myRef = React.createRef();
 
-  toggle = () => {
-    this.setState(
-      {
-        showModal: true,
-      },
-      () => {
-        // var videoSrc = 'http://192.168.100.1:9981/playlist/channels.m3u';
-        var videoSrc = "http://192.168.100.1:9981/stream/channelid/2120785922?ticket=A1D9A7E65992ADC1F93E8B70B848FD32C4118606&profile=pass";
-        // var videoSrc = src;
-        if (Hls.isSupported()) {
-          var hls = new Hls({xhrSetup: (xhr) => {
-            xhr.setRequestHeader("Access-Control-Allow-Origin", "http://127.0.0.1:1212");
-          }});
-          hls.loadSource(videoSrc);
-          hls.attachMedia(this.myRef.current);
-        }
-      }
-    );
-  };
+//   toggle = () => {
+//     this.setState(
+//       {
+//         showModal: true,
+//       },
+//       () => {
+//         var videoSrc = 'http://192.168.100.1:9981/playlist/channels.m3u';
+//         // var videoSrc = "http://192.168.100.1:9981/stream/channelid/2120785922?ticket=A1D9A7E65992ADC1F93E8B70B848FD32C4118606&profile=pass";
+//         // var videoSrc = src;
+//         if (Hls.isSupported()) {
+//           // var hls = new Hls({xhrSetup: (xhr) => {
+//           //   xhr.setRequestHeader("Access-Control-Allow-Origin", "http://172.20.10.4");
+//           // }});
+//         // if (Hls.isSupported()) {
+//           var hls = new Hls();
+//           hls.loadSource(videoSrc);
+//           hls.attachMedia(this.myRef.current);
+//           hls.on(Hls.Events.MEDIA_ATTACHING, (event, data) => {
 
-  render() {
-    return (
-      <>
-        <button onClick={this.toggle}>Toggle</button>
-        <br />
-        {this.state.showModal && (
-          <video
-            autoPlay
-            controls
-            ref={this.myRef}
-            style={{ height: 300 }}
-          ></video>
-        )}
-      </>
-    );
-  }
-}
+//           });
+//         }
+//       }
+//     );
+//   };
 
-export default App;
+//   render() {
+//     return (
+//       <>
+//         <button onClick={this.toggle}>Toggle</button>
+//         <br />
+//         {this.state.showModal && (
+//           <video
+//             autoPlay
+//             controls
+//             ref={this.myRef}
+//             style={{ height: 300 }}
+//           ></video>
+//         )}
+//       </>
+//     );
+//   }
+// }
+
+// export default App;
--- src/player.jsx
+++ src/player.jsx
@@ -1,23 +1,216 @@
+import { useEffect, useRef, useState } from 'react';
 import 'video.js/dist/video-js.css';
-import { Player } from 'video-react';
+import { mp4, mp2t } from "mux.js"
+// import { Player } from 'video-react';
 
+// import { useEffect, useRef } from 'react';
+// // import { fromFetch } from 'fetch-readablestream';
+// import { MuxDemux } from 'mux.js';
+
+// const VideoPlayer = () => {
+//   const videoRef = useRef(null);
+//   const mediaSourceRef = useRef(null);
+
+//   useEffect(() => {
+//     const video = videoRef.current;
+//     const mediaSource = new MediaSource();
+//     mediaSourceRef.current = mediaSource;
+
+//     fetch("http://192.168.100.1:9981/stream/channelid/1716352676?ticket=BBE208F603618773F2A9529E4C22E84A77D7ECB1&profile=pass")
+//       // .then(res => fromFetch(res.body))
+//       .then(stream => {
+//         const muxDemux = new MuxDemux({
+//           transmuxPipeline: {
+//             type: 'mp4'
+//           },
+//         });
+
+//         muxDemux.feed(stream);
+
+//         muxDemux.on('data', (chunk) => {
+//           mediaSource.appendBuffer(chunk.data);
+//         });
+
+//         mediaSource.addEventListener('sourceopen', () => {
+//           video.src = URL.createObjectURL(mediaSource);
+//           video.play();
+//         });
+//       });
+
+//     return () => {
+//       mediaSource.close();
+//       URL.revokeObjectURL(video.src);
+//     };
+//   }, []);
+
+//   return <video ref={videoRef} />;
+// };
 
 const VideoPlayer = () => {
-   return (
-     <>
-       <Player
-         playsInline
-         type="application/x-mpegURL"
-        //  src={props.src}
-         src="http://192.168.100.1:9981/stream/channelid/1716352676?ticket=BBE208F603618773F2A9529E4C22E84A77D7ECB1&profile=pass"
-         fluid={false}
-         width={720}
-         height={300}
-         playbackRate={[0.5, 1, 1.5, 2]}
-       />
-      </>
-   );
-};
+  const [segments, setSegments] = useState([]);
+  const flag = useRef(false);
+    // [
+    // // "http://192.168.100.1:9981/stream/channelid/1716352676?ticket=BBE208F603618773F2A9529E4C22E84A77D7ECB1&profile=pass"
+    // ]
+  // );
+
+  // Replace this value with your files codec info
+  let mime = 'video/mp4; codecs="mp4a.40.2,avc1.64001f"';
+
+  let mediaSource = useRef(new MediaSource());
+  let transmuxer = useRef(new mp4.Transmuxer());
+
+  let sourceBuffer = useRef(null);
+
+  const video = useRef(null);
+  useEffect(() => {
+    fetch("http://192.168.100.1:9981/stream/channelid/1716352676?ticket=BBE208F603618773F2A9529E4C22E84A77D7ECB1&profile=pass")
+    .then((response) => response.body)
+    .then((rb) => {
+    const reader = rb.getReader();
+
+    return new ReadableStream({
+      start(controller) {
+
+        // The following function handles each data chunk
+        function push() {
+          // "done" is a Boolean and value a "Uint8Array"
+          reader.read().then(({ done, value }) => {
+            // If there is no more data to read
+            if (done) {
+              console.log("done", done);
+              controller.close();
+              return;
+            }
+            // Get the data and send it to the browser via the controller
+            controller.enqueue(value);
+            setSegments((v) => {
+              v.push(value);
+              return v;
+            });
+            // Check chunks by logging to the console
+            // console.log(done, value);
+            push();
+          });
+        }
+
+        push();
+      },
+    });
+  })
+  .then((stream) =>
+    // Respond with our stream
+    new Response(stream, { headers: { "Content-Type": "text/html" } }).text(),
+  )
+  .then((result) => {
+    // Do things with result
+    console.log(result);
+  });
+    video.current.src = URL.createObjectURL(mediaSource.current);
+  },
+  []
+);
+
+  useEffect(() => {
+    console.log(segments.length);
+    if (segments.length != 0 && !flag.current) {
+      flag.current = true;
+      mediaSource.current.addEventListener("sourceopen", appendFirstSegment);
+      console.log("flag")
+    }
+  }, [segments]);
+
+function appendFirstSegment(){
+  console.log("gholam ");
+  if (segments.length == 0){
+    console.log("empty!")
+    return;
+  }
+  
+  URL.revokeObjectURL(video.src);
+  sourceBuffer.current = mediaSource.current.addSourceBuffer(mime);
+  sourceBuffer.current.addEventListener('updateend', appendNextSegment);
+
+  transmuxer.current.on('data', (segment) => {
+    console.log("gholam 1.5");
+    let data = new Uint8Array(segment.initSegment.byteLength + segment.data.byteLength);
+    data.set(segment.initSegment, 0);
+    data.set(segment.data, segment.initSegment.byteLength);
+    console.log(mp4.tools.inspect(data));
+    sourceBuffer.current.appendBuffer(data);
+    // reset the 'data' event listener to just append (moof/mdat) boxes to the Source Buffer
+    transmuxer.current.off('data');
+  })
+  console.log("gholam 2");
+  
+  transmuxer.current.push(segments[0]);
+  setSegments((v) => {
+    v.shift()
+    return v;
+  })
+  transmuxer.current.push();
+  transmuxer.current.flush();
+  
+    // fetch(segments.current.shift()).then((response)=>{
+  //     return response.arrayBuffer();
+  //   }).then((response) => {
+  //     transmuxer.current.push(new Uint8Array(response));
+  //     transmuxer.current.flush();
+  //   })
+  }
+
+  function appendNextSegment(){
+    transmuxer.current.on('data', (segment) =>{
+      sourceBuffer.current.appendBuffer(new Uint8Array(segment.data));
+      transmuxer.current.off('data');
+    })
+
+    if (segments.length == 0){
+      // notify MSE that we have no more segments to append.
+      mediaSource.endOfStream();
+    }
+
+    segments.forEach((segment) => {
+      transmuxer.current.push(segment);
+      transmuxer.current.flush();
+      // fetch the next segment from the segments array and pass it into the transmuxer.push method
+      // fetch(segments.current.shift()).then((response)=>{
+      //   return response.arrayBuffer();
+      // }).then((response)=>{
+
+      // })
+    })
+  }
+
+  return (
+    <>
+      <video ref={video} 
+        controls 
+        width="80%">
+      </video>
+    </>
+  )
+}
+
+// export default VideoPlayer;
+
+// const VideoPlayer = () => {
+//    return (
+//      <>
+//        <Player
+//          playsInline
+//         //  type="application/x-mpegURL"
+//         type="video/mp2t"
+//         //  src={props.src}
+//          src="http://192.168.100.1:9981/stream/channelid/1716352676?ticket=BBE208F603618773F2A9529E4C22E84A77D7ECB1&profile=pass"
+//          fluid={false}
+//          width={720}
+//          height={300}
+//          playbackRate={[0.5, 1, 1.5, 2]}
+//        />
+//       </>
+//    );
+// };
 
 // import { useRef } from 'react';
 // import VideoJS from 'react-video-js-player';
